33d8792aa70f9e7db922a7c4a1da432681ea53fc
Laura Kendl <lkendl@live.com>
2022-09-02 22:30:08 -0600

    added styling modules

--- v14/nodebucket/src/app/app.module.ts
+++ v14/nodebucket/src/app/app.module.ts
@@ -39,11 +39,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MessageModule } from 'primeng/message';
 import { MessagesModule } from 'primeng/messages';
 
@@ -71,10 +75,14 @@ import { MessagesModule } from 'primeng/messages';
     MatCardModule,
     MatDialogModule,
     MatDividerModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatGridListModule,
+    MatListModule,
     MatMenuModule,
+    MatTooltipModule,
     MatToolbarModule,
     MessageModule,
     MessagesModule,
